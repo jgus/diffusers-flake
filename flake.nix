117e4f58d02683dff8927edee94db3b39ba18535
Josh Gustafson <joshgstfsn@gmail.com>
2026-08-28 16:16:18 -0600

    Initial: diffusers flake

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "diffusers: version-bumped ahead of nixpkgs through a Python package overlay.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-lib = {
+      url = "github:jgus/flake-lib/v1";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, flake-lib, ... }:
+    let
+      pin = import ./pin.nix;
+      inherit (pin) version hash;
+      source = { type = "pypi"; pname = "diffusers"; format = "sdist"; };
+      overlay = final: prev: {
+        pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+          (pyfinal: pyprev: {
+            diffusers = pyprev.diffusers.overridePythonAttrs (_: {
+              inherit version;
+              doCheck = false;
+              src = pyfinal.fetchPypi { inherit version hash; pname = "diffusers"; };
+            });
+          })
+        ];
+      };
+    in
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ overlay ];
+          };
+        in
+        {
+          packages = {
+            diffusers = pkgs.python3.pkgs.diffusers;
+            default = pkgs.python3.pkgs.diffusers;
+            update-version = flake-lib.lib.mkUpdateVersion { inherit pkgs source; buildAttr = "diffusers"; };
+            update-branches = flake-lib.lib.mkUpdateBranches { inherit pkgs source; pinSchema = "pypi"; };
+          };
+        }) // {
+      overlays.default = overlay;
+    };
+}
